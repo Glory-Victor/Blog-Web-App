45751c34a82475b970af3fba3c7da664e596a9b9
Glory Victor <glory.victor@full.co>
2017-07-24 11:49:23 +0530

    Changed

--- templates/blog.py
+++ templates/blog.py
@@ -1,17 +1,23 @@
 import cgi
 import wsgiref.handlers
-from google.appengine.api import users
 import webapp2
  
 MAIN_HTML = open('main.html').read()
 NEW_HTML = open('new.html').read()
-dic = {}
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-
-        self.response.write(MAIN_HTML)
-               
+	self.response.write(MAIN_HTML)
+        self.response.out.write('<html><body>')
+        for key in dic:
+            self.response.out.write('<br>Title: <pre>')
+            self.response.out.write(key)
+            self.response.out.write('<br></pre>Content: <pre>')
+            self.response.out.write(dic[key])
+            self.response.out.write('<br>')
+        self.response.out.write('<br></pre>')
+        self.response.out.write('<br></pre></body></html>')
+	    
     def post(self):
         self.response.write(MAIN_HTML)
         self.response.out.write('<html><body>')
@@ -23,12 +29,13 @@ class MainPage(webapp2.RequestHandler):
             self.response.out.write('<br>')
         self.response.out.write('<br></pre>')
         self.response.out.write('<br></pre></body></html>')
-        
            
 class NewPage(webapp2.RequestHandler):
     def get(self):
         
         self.response.write(NEW_HTML)
+
+dic = {}
         
 class BlogItPage(webapp2.RequestHandler):
     def post(self):
@@ -37,7 +44,7 @@ class BlogItPage(webapp2.RequestHandler):
                                 </head>
                                 <body>Title: <pre>""")
         self.response.out.write(cgi.escape(self.request.get('title')))
-        self.response.out.write('</pre>Content: <pre>')
+        self.response.out.write('</pre><br>Content: <pre>')
         self.response.out.write(cgi.escape(self.request.get('content')))
         self.response.out.write("""</pre><form action="/mainpage" method="post">
                                 <br><div>
